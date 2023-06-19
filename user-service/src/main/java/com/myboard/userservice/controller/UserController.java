28a4a42f887e3122ae1564618bdf3eadeacaf813
thirdindex <thirdindex@gmail.com>
2023-06-20 02:55:01 +0530

    User creation and signup

--- user-service/src/main/java/com/myboard/userservice/controller/UserController.java
+++ user-service/src/main/java/com/myboard/userservice/controller/UserController.java
@@ -1,34 +1,73 @@
 package com.myboard.userservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.myboard.userservice.dto.UserSignupDTO;
-import com.myboard.userservice.service.UserService;
+import com.myboard.userservice.dto.LoginRequest;
+import com.myboard.userservice.dto.LoginResponse;
+import com.myboard.userservice.dto.SignupRequest;
+import com.myboard.userservice.entity.User;
+import com.myboard.userservice.repository.UserRepository;
+import com.myboard.userservice.security.JwtUtil;
 
 @RestController
 @RequestMapping("v1/users/")
 public class UserController {
 
-	private final UserService userService;
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
 
-	public UserController(UserService userService) {
-		this.userService = userService;
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	public UserController(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@PostMapping("/signup")
-	public ResponseEntity<?> signup(@RequestBody UserSignupDTO userSignupDTO) {
-		// This is where you'll call the UserService to handle signups
-		// userService.signup(userSignupDTO);
-		// Then you return the appropriate response
-		// return new ResponseEntity<>(userService.signup(userSignupDTO),
-		// HttpStatus.CREATED);
-		// Currently just returning a placeholder response
-		return ResponseEntity.ok("Signup endpoint hit");
+	public ResponseEntity<String> signup(@RequestBody SignupRequest signupRequest) {
+		// Validate signup request (e.g., check if username is available, password
+		// requirements)
+
+		// Check if a user with the same username already exists
+		if (userRepository.findByUsername(signupRequest.getUsername()) != null) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists");
+		}
+
+		// Create a new user entity and populate its fields
+		User user = new User();
+		user.setUsername(signupRequest.getUsername());
+		String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
+		user.setPassword(encodedPassword);
+
+		// Save the user in the database
+		userRepository.save(user);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+		User user = userRepository.findByUsername(loginRequest.getUsername());
+
+		if (user == null || !passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+		}
+
+		//String token = jwtUtil.generateToken(user.getUsername());
+
+		// Create a response object containing the token and user details
+		LoginResponse response = new LoginResponse("", user);
+
+		return ResponseEntity.ok(response);
 	}
 
 	@GetMapping("/hello")
--- user-service/src/main/java/com/myboard/userservice/security/SecurityConfig.java
+++ user-service/src/main/java/com/myboard/userservice/security/SecurityConfig.java
@@ -3,15 +3,20 @@ package com.myboard.userservice.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		//http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated());
+		http.authorizeHttpRequests((authorize) -> authorize.anyRequest().permitAll());
+		http.csrf().disable();
 		return http.build();
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
