48182f1184c8a45ce328d60410be435f7bbbe810
VipnChandranPalangate <vipin.chandranp@gmail.com>
2024-04-07 00:56:24 +0530

    Save

---
+++ main-api/src/main/java/com/myboard/userservice/entity/RoutePoints.java
@@ -0,0 +1,11 @@
+package com.myboard.userservice.dto;
+
+import lombok.Data;
+
+@Data
+public class RoutePointsDTO {
+    private double latitude;
+    private double longitude;
+
+    // Constructors, getters, and setters
+}
